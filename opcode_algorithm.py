97a8c4c3b634319ebe84a54cfbfd974fe3aad5e4
Leonard Eyer <leonardeyer@fastmail.de>
2024-03-04 18:07:11 +0100

    make fingerprinting work for some test dumps

--- opcode_algorithm.py
+++ opcode_algorithm.py
@@ -1,14 +1,10 @@
-def opcode_fingerprinting(opcode,N=100):
-    #opcode is a list of different opcodes
-    if N>=0:
-        OCSet=set()
-        CR=opcode[0]
-        SR=opcode[1]
-        i=2
-        while i!= N & i< len(opcode):
-            if opcode[i] in [CR, SR] and len(OCSet)>=4:
-                return False
-            OCSet.add(opcode[i])
-            i += 1
-        print(OCSet)
-        return (i ==N) & (4 <= len(OCSet)<= 10)
+def opcode_fingerprinting(opcodes):
+    # opcodes is a list of different opcodes
+    CR=opcodes[0]
+    SR=opcodes[1]
+    OCSet=set([CR, SR])
+    for opcode in opcodes:
+        if opcode in [CR, SR] and len(OCSet)>=4:
+            return False
+        OCSet.add(opcode)
+    return 4 <= len(OCSet) <= 10
--- passive_opcode.py
+++ passive_opcode.py
@@ -1,32 +1,22 @@
 from scapy.all import rdpcap
 from scapy.layers.inet import UDP
-# import cryptography
 import opcode_algorithm
 
-'''
-Structure of encrypted TLS data depends on negotiated cipher suite
-- AES_GCM (most common in the pcaps) => starts with 8 byte nonce NOT part of encrypted data
-- CBC modes (common in chat files) => starts with 16 byte IV also NOT part of encrypted data
-Sometimes this is not the case. Not sure why.
-ref:
-https://security.stackexchange.com/questions/187924/tls1-2-aes-128-cbc-encrypted-data-size
-https://security.stackexchange.com/questions/136180/tls-1-2-and-enable-only-aead-ciphers-suite-list
-https://security.stackexchange.com/questions/54466/in-ssl-tls-what-part-of-a-data-packet-is-encrypted-and-authenticated
-'''
 
-N = 100
-
-if __name__ == "__main__":
-    # load_layer("tls")
+def flag_openvpn_in_capture(filename):
     opcodes = []
-    file = rdpcap('openvpn-server/dump.pcap')
+    file = rdpcap(filename)
     for packet in file:
         if UDP in packet:
             # application data packets
             packet_udp:UDP = packet[UDP]
             payload = bytes(packet_udp.payload)
-            # print(payload)
             opcode = (payload[0] & 0b11111000) >> 3
-            print(opcode)
             opcodes.append(opcode)
-    print(opcode_algorithm.opcode_fingerprinting(opcodes, len(opcodes)))
+    return opcode_algorithm.opcode_fingerprinting(opcodes)
+
+
+if __name__ == "__main__":
+    print("Flagged: ", flag_openvpn_in_capture("pcap-dumps/mullvad-ovpn-bridge-mode.pcap"))
+    print("Flagged: ", flag_openvpn_in_capture("pcap-dumps/synthesized-openvpn-server-dump.pcap"))
+    print("Flagged: ", flag_openvpn_in_capture("pcap-dumps/non-vpn.pcap"))
