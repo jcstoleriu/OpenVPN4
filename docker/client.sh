c30721b71db3b0f0d03750a96ec30ec1f3c991a5
MatthijsReyers <matthijscrafter@gmail.com>
2024-03-18 12:41:47 +0100

    Created automatic network capture setup

---
+++ docker/client.sh
@@ -0,0 +1,12 @@
+# This script runs in all client containers and serves primarly to generate some network traffic.
+
+# Wait for OpenVPN client to connect.
+sleep 10
+
+echo "Generating network traffic.."
+
+curl -s "https://google.com/" > google.html
+
+ping -c 3 8.8.8.8
+
+curl -s "https://www.tudelft.nl/" tudelft.html
