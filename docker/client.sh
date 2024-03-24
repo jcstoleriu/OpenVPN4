33ab6f0d522ac79a24e137320fc9e17a8ba2cbdf
matthijsreyers <matthijscrafter@gmail.com>
2024-03-24 16:12:45 +0100

    Added some more network traffic to client script

--- docker/client.sh
+++ docker/client.sh
@@ -5,7 +5,14 @@ sleep 10
 
 echo "Generating network traffic.."
 
+ping -c 2 facebook.com
+
 curl -s "https://google.com/" > google.html
+
+curl -s "https://www.google.nl/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png" > logo.png
+curl -s "https://www.google.nl/images/searchbox/desktop_searchbox_sprites318_hr.webp" > sprite.webp
+
+ping -c 2 youtube.com
 
 ping -c 3 8.8.8.8
 
