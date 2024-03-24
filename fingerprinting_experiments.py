006bc91f68f0211f05fab003cd0b4339c8e12dbb
Tobias loch <Tobias.loch@mail.de>
2024-03-24 14:21:00 +0100

    changing config to include more pcaps ignoring now files which are larger than 2 gigs

--- fingerprinting_experiments.py
+++ fingerprinting_experiments.py
@@ -14,8 +14,9 @@ OUTPUT_CSV_HEADER = ["name", "file", "algorithm", "ip1", "port1", "ip2", "port2"
 DEFAULT_CONFIG_PATH = "config.json"
 DEFAULT_OUTPUT_FILE = "output.csv"
 PARAMS_KEY = "params"
+MAX_FILE_SIZE_KEY = "max_file_size"
 
-def main(argv):
+def main(argv:list):
     config_path = DEFAULT_CONFIG_PATH
     dry_run = False
 
@@ -41,6 +42,10 @@ def main(argv):
             logging.StreamHandler(sys.stdout)
         ]
     )
+
+        # give a limit for the max file size
+    max_file_size = config[MAX_FILE_SIZE_KEY] if MAX_FILE_SIZE_KEY in config else float("inf")
+    logging.info(f"Max file size is {max_file_size}")
 
     with open(output_file, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, delimiter=',',
@@ -68,6 +73,10 @@ def main(argv):
             for j, file in enumerate(experiment_datasets):
                 if not os.path.exists(file):
                     logging.error(f"File {file} does not exist. Skipping it.")
+                    continue
+                filesize = os.path.getsize(file)
+                if filesize > max_file_size:
+                    logging.error(f"Skipping file {file}, due to large filesize {filesize} (limit {max_file_size})")
                     continue
                 logging.info(f"Fingerprinting file '{file}' ({j+1} of {len(experiment_datasets)} files)...")
                 
