99d637578b14d28032c6df75e2a53277a0df65ad
Tobias loch <Tobias.loch@mail.de>
2024-03-25 09:14:04 +0100

    the experiments are now split into each dataset, such that if an error occurs they can be easier restarted

--- fingerprinting_experiments.py
+++ fingerprinting_experiments.py
@@ -12,9 +12,11 @@ OUTPUT_CSV_HEADER = ["name", "file", "algorithm", "ip1", "port1", "ip2", "port2"
 
 
 DEFAULT_CONFIG_PATH = "config.json"
-DEFAULT_OUTPUT_FILE = "output.csv"
+DEFAULT_OUTPUT_FOLDER = "experiments"
 PARAMS_KEY = "params"
 MAX_FILE_SIZE_KEY = "max_file_size"
+
+TEMP_FILE_EXTENSION = ".tmp"
 
 def main(argv:list):
     config_path = DEFAULT_CONFIG_PATH
@@ -32,70 +34,81 @@ def main(argv:list):
 
     # parse datasets
     config_datasets = config["datasets"]
+    experiments = config["experiments"]
+    output_folder = config.get("output_folder", DEFAULT_OUTPUT_FOLDER)
+    os.makedirs(output_folder, exist_ok=True)
 
-    output_file = config.get("output", DEFAULT_OUTPUT_FILE)
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
         handlers=[
-            logging.FileHandler(os.path.splitext(output_file)[0] + ".log", mode='w'),
-            logging.StreamHandler(sys.stdout)
+            logging.StreamHandler(sys.stdout),
+            logging.FileHandler(os.path.join(output_folder, "experiments.log"), mode='a')
         ]
     )
 
-        # give a limit for the max file size
-    max_file_size = config[MAX_FILE_SIZE_KEY] if MAX_FILE_SIZE_KEY in config else float("inf")
-    logging.info(f"Max file size is {max_file_size}")
+    used_datasets = config["used_datasets"]
+    for dataset_key in used_datasets:
+        output_file_basename = os.path.join(output_folder, dataset_key)
+        if len(logging.getLogger().handlers) > 2:
+            logging.getLogger().handlers.pop(2)
+        logging.getLogger().addHandler(logging.FileHandler(f"{output_file_basename}.log", mode='w'))
 
-    with open(output_file, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(OUTPUT_CSV_HEADER)
+        output_file = output_file_basename + ".csv"
+        if os.path.exists(output_file):
+            logging.info(f"Output file {output_file} already exists. Skipping it.")
+            continue
+        output_file = output_file + TEMP_FILE_EXTENSION
 
-        used_datasets = config["used_datasets"]
-        dataset_files = []
-        for experiment_dataset_key in used_datasets:
-            if experiment_dataset_key in config_datasets:
-                dataset_files += config_datasets[experiment_dataset_key]
-            else:
-                logging.error(f"dataset {experiment_dataset_key} not found in config. Skipping it.")
-        
-        experiments = config["experiments"]
-        # run all experiments
-        for j, file in enumerate(dataset_files):
+        if not dataset_key in config_datasets:
+            logging.error(f"dataset {dataset_key} not found in config. Skipping it.")
+            continue
+        for j, file in enumerate(config_datasets[dataset_key]):
             if not os.path.exists(file):
                 logging.error(f"File {file} does not exist. Skipping it.")
                 continue
+            
 
+            # give a limit for the max file size
+            max_file_size = config[MAX_FILE_SIZE_KEY] if MAX_FILE_SIZE_KEY in config else float("inf")
+            logging.info(f"Max file size is {max_file_size}")
             filesize = os.path.getsize(file)
             if filesize > max_file_size:
                 logging.error(f"Skipping file {file}, due to large filesize {filesize} (limit {max_file_size})")
                 continue
 
-            logging.info(f"Fingerprinting file '{file}' ({j+1} of {len(dataset_files)} files)...")
-            if not dry_run:
-                packets = PcapReader(file)
-                conversations, _ = group_conversations(packets, progressbar=True)
+            with open(output_file, 'w', newline='') as csvfile:
+                csv_writer = csv.writer(csvfile, delimiter=',',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerow(OUTPUT_CSV_HEADER)
 
-            for i, experiment in enumerate(experiments):
-                experiment_name = experiment["name"]
-                algorithm_type = experiment["algorithm"]
-                algorithm = ALGORITHMS[algorithm_type]
-                logging.info(f"Running experiment '{experiment_name}' with algorithm {algorithm_type} ({i+1} of {len(experiments)} experiments)")
-                
-                params = experiment.get(PARAMS_KEY, None)
+                # run all experiments
+                logging.info(f"Fingerprinting file '{file}' ({j+1} of {len(config_datasets[dataset_key])} files)...")
+                if not dry_run:
+                    packets = PcapReader(file)
+                    conversations, _ = group_conversations(packets, progressbar=True)
 
-                if dry_run:
-                    continue
+                for i, experiment in enumerate(experiments):
+                    experiment_name = experiment["name"]
+                    algorithm_type = experiment["algorithm"]
+                    algorithm = ALGORITHMS[algorithm_type]
+                    logging.info(f"Running experiment '{experiment_name}' with algorithm {algorithm_type} ({i+1} of {len(experiments)} experiments)")
+                    
+                    params = experiment.get(PARAMS_KEY, None)
 
-                results = algorithm(file, conversations=conversations, params=params, printer=lambda x : logging.info(x))
+                    if dry_run:
+                        continue
 
-                # print(f"Results for experiment {experiment['name']}")
-                # print(results)
+                    results = algorithm(file, conversations=conversations, params=params, printer=lambda x : logging.info(x))
 
-                for key, result in results.items():
-                    csv_writer.writerow([experiment_name, file, algorithm_type, key[0][0], key[0][1], key[1][0], key[1][1], result])
-    logging.info(f"output written to {output_file}")
+                    # print(f"Results for experiment {experiment['name']}")
+                    # print(results)
+
+                    for key, result in results.items():
+                        csv_writer.writerow([experiment_name, file, algorithm_type, key[0][0], key[0][1], key[1][0], key[1][1], result])
+        output_file_final = output_file.replace(TEMP_FILE_EXTENSION, "")
+        os.rename(output_file, output_file_final)
+        logging.info(f"output written to {output_file_final}")
 
 if __name__ == "__main__":
     main(sys.argv)
