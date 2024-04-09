d3a67bcd9fb82884f06cf1b7d310fb837ce78785
Tobias loch <Tobias.loch@mail.de>
2024-04-09 13:15:02 +0200

    added active fingerprinting to the opcode script

--- openvpn_fingerprinting.py
+++ openvpn_fingerprinting.py
@@ -4,9 +4,10 @@ from scapy.layers.inet import UDP, IP, TCP
 import opcode_algorithm
 import ack_algorithm
 from utils import group_conversations, print_summary
-import sys, tqdm
+import sys, tqdm, argparse
+import active_fingerprinting
 
-def flag_openvpn_in_capture(filename, params={}):
+def flag_openvpn_in_capture(filename, params={}, use_active_fingerprinting=False):
     packets = PcapReader(filename)
     print(f"Reading file {filename}...")
     conversations = group_conversations(packets, progressbar=True)
@@ -15,12 +16,21 @@ def flag_openvpn_in_capture(filename, params={}):
 
     results = {}
     for key, packets_in_conversation in conversations.items():
-        results[key] = (results_opcode[key], results_ack[key])
+        opcode_result = results_opcode[key]
+        ack_result = results_ack[key]
+        
+        active_fingerprinting_result = None
+        if use_active_fingerprinting and (opcode_result or ack_result):
+            c1_result = active_fingerprinting.fingerprint(*key[0])
+            c2_result = active_fingerprinting.fingerprint(*key[1])
+            active_fingerprinting_result = (c1_result, c2_result)
+
+        results[key] = (opcode_result, ack_result, active_fingerprinting_result)
 
     return results, conversations
 
 
-def main(argv):
+def main():
     files = [
         # "docker-pcaps/udp.pcap",
         "docker-pcaps/tcp.pcap",
@@ -29,27 +39,35 @@ def main(argv):
         "pcap-dumps/non-vpn.pcap",
     ]
 
-    params = {}
-    if "-o" in argv:
-        i = argv.index("-o")
-        params[opcode_algorithm.XOR_OPCODES_KEY] = True
-        argv.pop(i)
+    parser = argparse.ArgumentParser(description="Flag OpenVPN connections in a pcap file")
+    parser.add_argument("files", nargs="+", default=files, help="The pcap files to analyze")
+    parser.add_argument("-o", action="store_true", help="Use XOR Optimization in the opcode algorithm")
+    parser.add_argument("--active-fingerprinting", action="store_true", help="Use active fingerprinting (default: False)")
 
-    if len(argv) > 1:
-        files = argv[1:]
+    args = parser.parse_args()
 
-    for file in files:
+    params = {opcode_algorithm.XOR_OPCODES_KEY: args.o}
+    for file in args.files:
         results, conversations = flag_openvpn_in_capture(file, params=params)
 
         items = list(results.items())
-        items.sort(key=lambda k : sum([int(v) for v in k[1]]))
+        items.sort(key=lambda k : sum([int(v) for v in k[1][:2]]))
         for (ip1, ip2), result in items:
             print(f"\nconversation between {ip1} and {ip2}")
             
             print(f"Opcode algorithm flag result: {result[0]}")
             print(f"ACK algorithm flag result: {result[1]}")
-        
+
+            active_fingerprinting_result = result[2]
+            if not active_fingerprinting_result is None:
+                if active_fingerprinting_result[0]:
+                    print(f"{ip1} was flagged as VPN by the active fingerprinting algorithm!")
+                if active_fingerprinting_result[1]:
+                    print(f"{ip2} was flagged as VPN by the active fingerprinting algorithm!")
+                if not (active_fingerprinting_result[0] or active_fingerprinting_result[1]):
+                    print("No VPN flag by the active fingerprinting algorithm")
+
         print_summary(file, conversations, items)
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
